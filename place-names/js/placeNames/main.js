c63108d5409a1d14e20d5888d7758715c3fa8601
JavontaeH <hardeman.javontae@gmail.com>
2022-05-20 14:19:40 -0500

    finished placeNames

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,8 +1,14 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
 console.log("All Pace Names");
@@ -10,3 +16,8 @@ console.log("All Pace Names");
 for (const name of names) {
   console.log(name);
 }
+
+console.log();
+console.log("The Place Names");
+theNames = names.filter((name) => name.startsWith("The"));
+theNames.forEach((name) => console.log(name));
