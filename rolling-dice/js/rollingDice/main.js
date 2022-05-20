897c658c2096e84effe1150461a40547524cdcb8
JavontaeH <hardeman.javontae@gmail.com>
2022-05-20 15:04:09 -0500

    finished roll dice

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,58 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+class Die {
+  constructor(value) {
+    this.value = value;
+    this.dieString = "Unknown";
+  }
+
+  roll() {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    this.value = getRandomInt(1, 6);
+    this.dieString = this.value;
+    switch (this.value) {
+      case 1:
+        this.dieString = "one";
+        break;
+      case 2:
+        this.dieString = "two";
+        break;
+      case 3:
+        this.dieString = "three";
+        break;
+      case 4:
+        this.dieString = "four";
+        break;
+      case 5:
+        this.dieString = "five";
+        break;
+      case 6:
+        this.dieString = "six";
+        break;
+    }
+  }
+}
+
+let die1 = new Die();
+let die2 = new Die();
+
+for (var i = 0; i < 10; i++) {
+  die1.roll();
+  die2.roll();
+
+  message = `${die1.dieString} + ${die2.dieString} == ${
+    die1.value + die2.value
+  } `;
+  if (die1.value == die2.value) {
+    message += " DOUBLES!";
+  }
+
+  console.log(message);
+}
